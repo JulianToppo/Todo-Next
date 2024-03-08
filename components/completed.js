0524c55488c60ca23f319a7515b2de868ec269bd
JulianToppo <juliantoppo95@gmail.com>
2024-03-08 12:02:37 +0530

    Showing completed tasks

--- components/completed.js
+++ components/completed.js
@@ -4,12 +4,13 @@ import { useSelector } from "react-redux";
 const Completed = (props) => {
   const { todoItems } = props;
   return (
-    <div className="bg-green-400 flex w-full h-svh items-center justify-center">
-      <div className="flex flex-col w-1/2 p-7 bg-yellow-200 rounded-lg">
+    <div className="bg-green-400 bg-opacity-10 flex w-full h-svh items-center justify-center">
+      <div className="flex flex-col w-1/2  bg-yellow-200 rounded-lg space-y-2 p-2">
+        <h1 className="self-center font-medium text-xl">Tasks Completed:</h1>
         {todoItems &&
           todoItems.map((item) => {
             if (item.completed) {
-              return <li key={item._id}>{item.name}</li>;
+              return <div className="bg-green-200 p-2 rounded-md" key={item._id}>{item.name}</div>;
             }
           })}
       </div>
